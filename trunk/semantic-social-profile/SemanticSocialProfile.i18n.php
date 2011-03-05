ff0abb1517f484aac83fab70ec74292571452ad9
ganqTurgon <ganqTurgon@0c96a6dd-a22d-37a5-0475-aaa3db6fb5f6>
2011-03-05 13:43:39 +0000

    first draft of setup. That's basicaly the issue number #10

---
+++ trunk/semantic-social-profile/SemanticSocialProfile.i18n.php
@@ -0,0 +1,15 @@
+<?php
+$messages = array();
+
+/* *** English *** */
+$messages['en'] = array(
+    'ssp' => 'Semantic Social Profile',
+    'ssp-desc' => "Extension's description",
+    'ssp-setupsspbutton' => "Setup Semantic Social Profile",
+    );
+
+/* *** Ukrainian (Український) *** */
+$messages['ua'] = array(
+    'ssp' => 'Семантичний Соцiальний Профиль',
+    'ssp-desc' => 'Це дуже гарненьке розширення дозволяє представляти соціальний профіль як семантични властивости',
+    );
