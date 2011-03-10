c22be121fb8f72293ef3c7b3e4ebe44f305da89e
zebolasha <zebolasha@gmail.com>
2011-03-10 23:46:32 +0000

    the changes are corresponding the following issues: 1) http://code.google.com/p/semantic-social-profile/issues/detail?id=16 2) http://code.google.com/p/semantic-social-profile/issues/detail?id=17 3)http://code.google.com/p/semantic-social-profile/issues/detail?id=19

--- SemanticSocialProfile/SemanticSocialProfile.i18n.php
+++ SemanticSocialProfile/SemanticSocialProfile.i18n.php
@@ -5,11 +5,25 @@ $messages = array();
 $messages['en'] = array(
     'ssp' => 'Semantic Social Profile',
     'ssp-desc' => "Extension's description",
-    'ssp-setupsspbutton' => "Setup Semantic Social Profile",
+    'ssp-setupsspbutton' => "Setup",
+    'semanticsocialprofile' => "Semantic Social Profile Setup",
+    'ssp-setupdesc' => "Press the button to setup the extension"
     );
 
-/* *** Ukrainian (Український) *** */
+/* *** Ukrainian (Українська) *** */
 $messages['ua'] = array(
-    'ssp' => 'Семантичний Соцiальний Профиль',
-    'ssp-desc' => 'Це дуже гарненьке розширення дозволяє представляти соціальний профіль як семантични властивости',
+    'ssp' => 'Семантичний Соцiальний Профіль',
+    'ssp-desc' => 'Це дуже гарненьке розширення дозволяє представляти соціальний профіль як семантичні властивості',
+    'ssp-setupsspbutton' => "Встановити",
+    'semanticsocialprofile' => "Встановлення Семантичного Соцiального Профілю",
+    'ssp-setupdesc' => "Натисніть кнопку щоб встановити розширення"
+    );
+
+/* *** Russian (Русский) *** */
+$messages['ru'] = array(
+    'ssp' => 'Семантический Социальный Профиль',
+    'ssp-desc' => 'Это расширение позволяет представлять социальный профиль как семантические свойства',
+    'ssp-setupsspbutton' => "Установить",
+    'semanticsocialprofile' => "Установка Семантического Социального Профиля",
+    'ssp-setupdesc' => "Нажмите кнопку чтоб установить расширение"
     );
--- SemanticSocialProfile/SemanticSocialProfile_body.php
+++ SemanticSocialProfile/SemanticSocialProfile_body.php
@@ -1,52 +1,99 @@
 <?php
 class SpecialSemanticSocialProfile extends SpecialPage {
   function __construct() {
-    parent::__construct( 'SemanticSocialProfile' );
+    parent::__construct( 'SemanticSocialProfile', 'editinterface' );
     wfLoadExtensionMessages('SemanticSocialProfile');
   }
 
   function execute( $par ) {
-    global $wgRequest, $wgOut;
-
+    global $wgRequest, $wgOut, $wgUser, $wgContLang;
+    
+    //checks if the user has a right to access the page
+		if ( !$this->userCanExecute($wgUser) ) {
+			$this->displayRestrictionError();
+			return;
+		}
+	
     $this->setHeaders();
 
 # Get request data from, e.g.
     $param = $wgRequest->getText('param');
     $action = $wgRequest->getText('action');
-    if ($action == 'setupssp') {
-      $this->wfsetUp('bla');
+    if ($wgRequest->wasPosted()) {
+      $this->wfsetUp();
+      $this->wfUpdateUserProfiles($wgContLang->getNsText(NS_USER));
     }
 
 # Do stuff
 # ...
-    $output="Hello world!";
+    $output=wfMsg('ssp-setupdesc');
 
-    $html = '<form name="setupssp" action="" method="POST">' .
-	    '<input type="hidden" name="action" value="setupssp" />' . "\n" .
-            '<input type="submit" value="'. wfMsg('ssp_setupsspbutton') .'"/>' . "\n".
+    $html = '<form name="setupssp" action="" method="POST">' . "\n".
+            '<input type="submit" value="'. wfMsg('ssp-setupsspbutton') .'"/>' . "\n".
 	    '</form>';
     $wgOut->addWikiText( $output);
     $wgOut->addHtml( $html);
   
   }
 
-  function wfsetUp($name){
-    global $wgOut;
+  function wfsetUp(){
+    global $wgOut, $wgContLang;
     $directory = dirname(__FILE__) . '/setup';
     $wgOut->addWikiText($directory);
-    echo ($directory);
+    //echo ($directory);
     //get the list of all files in a setup directory
     $filenames = scandir($directory);
     $summary = 'Semantic Social Profile installation procedure';
     foreach ($filenames as $filename) {
       if(is_file($directory.'/'.$filename)) {
-	$fn = urldecode($filename);
-	$wgOut->addWikiText($fn);
-	$pageTitle = Title::newFromText($fn);
+	$fn = explode('#',$filename,2);
+	$wgOut->addWikiText($fn[1]);
+	$pageTitle = Title::newFromText($wgContLang->getNsText($fn[0]).":".$fn[1]);
 	$page = new Article($pageTitle);
 	$page->doEdit(file_get_contents($directory.'/'.$filename), $summary);
 
       }
     }
     return true;
-  }}
+  }
+  
+	function wfUpdateUserProfiles($user_ns)
+	{
+		global $wgOut, $wgContLang;
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select(
+		'user',
+		array( 'user_name' ),
+		'user_id > 0',
+		__METHOD__
+		);
+		if($res){
+			//if users were found - prepare data
+			$summary = 'the userpage has been created using SSP Setup';
+			$id = Title::newFromText($wgContLang->getNsText( NS_TEMPLATE ).":Semantic_Social_Profile")->getArticleId();
+			$templatearticle = Article::newFromId( $id )->getRawText();
+			
+			$template = preg_replace("/^.*(?:<pre>)(.*)(?:<\/pre>).*$/isU","$1",$templatearticle, 1);
+			//$wgOut->addWikiText("users found");
+		}
+		
+		foreach( $res as $row ){
+			$pageTitle = Title::newFromText($user_ns.":".$row->user_name);
+			$id = $pageTitle->getArticleId();
+			if($id > 0){
+				// if the userpage already exists
+				// add template to the bottom
+				$page = Article::newFromId( $id );
+				if(strpos($page->getRawText(),"{{Semantic Social Profile") === false)
+					$page->updateArticle($page->getRawText().$template, '', false, false );
+			}
+			else {
+				// need to create a userpage with the SSP template
+				$page = new Article($pageTitle);
+				$page->doEdit($template, $summary );
+			}
+			//$wgOut->addWikiText($row->user_name." id: ".$id);
+		}
+		return true;
+	}
+}
